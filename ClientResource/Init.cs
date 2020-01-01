7018ec9ff81f66c97dc3aa726c5781719f104bdc
facebamm <dbaumerffg@web.de>
2020-01-01 04:23:39 +0100

    first commit

---
+++ ClientResource/Init.cs
@@ -0,0 +1,18 @@
+﻿using CitizenFX.Core;
+using static CitizenFX.Core.Native.API;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClientResource
+{
+    public class Init : BaseScript
+    {
+        public Init()
+        {
+
+        }
+    }
+}
